ad6f3ec72069314298d74e8037dad58cc0cfeb7e
Thiago Cabral <thiagocabral477@gmail.com>
2021-07-09 14:28:35 -0300

    created partials

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 3333
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views/'))
+app.set('views', path.join(__dirname, 'views/pages'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(routes)
