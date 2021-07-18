3bd4028710ce4df99ef9789e88bce1d016a95c6c
Thiago Cabral <thiagocabral477@gmail.com>
2021-07-18 16:16:43 -0300

    prece filter complete

---
+++ src/public/js/slider_filter.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector('.filtro__slider')
+const span = document.querySelector('.value__initial')
+
+slider.addEventListener('input', () => {
+  const linearValue = parseInt((slider.value - 500000) / 500000 * 100) + 100
+  slider.style.background = `linear-gradient(90deg, #401698 ${linearValue}%, #fff ${linearValue}%)`
+  const valueArr = slider.value.split('')
+
+  let value = slider.value
+
+  if (valueArr.length >= 4) {
+    const last = valueArr.splice((valueArr.length - 3), 3)
+    value = [...valueArr, ',', ...last].join('')
+  }
+
+  span.textContent = `R$${value}`
+})
