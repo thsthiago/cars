e11a62be7dab766eb23b249dc753856a9d4e0d63
Thiago Cabral <thiagocabral477@gmail.com>
2021-07-19 09:45:31 -0300

    adding card on sales page

--- src/controller/vendas.js
+++ src/controller/vendas.js
@@ -1,7 +1,26 @@
+const data = [
+  {
+    nome: 'Uno',
+    info: '1.6 8v, Gasolina, 2P, Manual',
+    image: 'https://images.pexels.com/photos/3802508/pexels-photo-3802508.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    nome: 'Pálio',
+    info: '1.6 8v, Gasolina, 2P, Manual',
+    image: 'https://images.pexels.com/photos/3802508/pexels-photo-3802508.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  },
+  {
+    nome: 'Fox',
+    info: '1.6 8v, Gasolina, 2P, Manual',
+    image: 'https://images.pexels.com/photos/3802508/pexels-photo-3802508.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  }
+]
+
 module.exports = {
   get (request, response) {
     return response.render('vendas', {
-      page: 'vendas'
+      page: 'vendas',
+      data
     })
   }
 }
